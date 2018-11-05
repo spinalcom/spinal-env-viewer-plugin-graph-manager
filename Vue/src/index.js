aed38ca19491134ff0ea463c35e64fa395d10208
Loris Ciprin <loris@ciprin.fr>
2018-11-05 16:43:58 +0100

    "remove externe css"

--- Vue/src/index.js
+++ Vue/src/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
 import Vuex from "vuex";
 
 Vue.config.productionTip = false;
