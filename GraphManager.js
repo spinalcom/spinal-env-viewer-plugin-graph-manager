25333e2d274ae6b09e542f6acdd56c84d3358d00
LorisC <loris@ciprin.fr>
2018-11-28 09:33:55 +0100

    [UPDATE] node appears directly when added

--- GraphManager.js
+++ GraphManager.js
@@ -20,34 +20,36 @@ export default class GraphManager {
                 if (this.nodes.length > 0)
                     store.dispatch("addContexts", this.nodes);
                 store.dispatch("retrieveGlobalBar", this.graph);
-                store.dispatch("setGraph", this.graph)
+                store.dispatch("setGraph", this.graph);
+                this.graph.bind(this.graphChange.bind(this));
+
             })
         }).bind(this));
 
-        this.graph.bind(this.graphChange.bind(this));
         store.subscribe((mutation, state) => {
             if (mutation.type === "PULL_CHILDREN") {
+                store.dispatch('emptyPoll', mutation.payload);
+
                 if (state.nodes.hasOwnProperty(mutation.payload))
                     state.nodes[mutation.payload].getChildren([])
                         .then(children => {
                             store.dispatch('addNodes', children);
-                            store.dispatch('emptyPoll', mutation.payload)
                         })
                         .catch(e => console.error(e));
             }
         })
     }
 
-      graphChange() {
-          this.graph.getChildren(['hasContext']).then(children => {
+    graphChange() {
+        this.graph.getChildren(['hasContext']).then(children => {
 
-              this.nodes.push(...children);
-              if (this.nodes.length > 0)
-                  this.store.dispatch("addContexts", this.nodes);
-              this.store.dispatch("retrieveGlobalBar", this.graph);
-              this.store.dispatch("setGraph", this.graph)
-          })
-      }
+            this.nodes.push(...children);
+            if (this.nodes.length > 0)
+                this.store.dispatch("addContexts", this.nodes);
+            this.store.dispatch("retrieveGlobalBar", this.graph);
+            this.store.dispatch("setGraph", this.graph)
+        })
+    }
 
 }
 
@@ -75,11 +77,4 @@ async function transformNode(result, context, node, set) {
     }
 
     return result;
-}
-
-
-function node2display(context, node) {
-    const set = new Set();
-
-    return transformNode({}, context, node, set)
 }
