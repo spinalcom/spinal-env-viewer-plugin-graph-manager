99685485b61498b09e9ff5f9e425f8d5904bebde
LorisC <loris@ciprin.fr>
2018-12-06 11:11:27 +0100

    [UPDATE] integrate graph service

---
+++ GraphManagerv1.js
@@ -0,0 +1,62 @@
+import {SpinalGraph} from 'spinalgraph';
+
+export default class GraphManager {
+  constructor(store) {
+    this.nodes = [];
+    this.store = store;
+
+    window.spinal.spinalSystem.getModel().then((function (forgeFile) {
+      if (!forgeFile.hasOwnProperty('graph')) {
+        forgeFile.add_attr({
+          graph: new SpinalGraph()
+        });
+      }
+      this.graph = forgeFile.graph;
+
+      this.graph.getChildren(['hasContext']).then(children => {
+        for (let i = 0; i < children.length; i++) {
+          if (children[i].info.name.get() !== 'BIMObjectContext'){
+            this.nodes.push(children[i]);
+            console.log(children[i].info.name.get(),children[i].info.name.get() === 'BIMObjectContext');
+          }
+        }
+        console.log(this.nodes);
+        if (this.nodes.length > 0)
+        {store.dispatch("addContexts", this.nodes);}
+        store.dispatch("retrieveGlobalBar", this.graph);
+        store.dispatch("setGraph", this.graph);
+        this.graph.bind(this.graphChange.bind(this));
+
+      });
+    }).bind(this));
+
+    store.subscribe((mutation, state) => {
+      if (mutation.type === "PULL_CHILDREN") {
+        store.dispatch('emptyPoll', mutation.payload);
+
+        if (state.nodes.hasOwnProperty(mutation.payload))
+        {state.nodes[mutation.payload].getChildren([])
+          .then(children => {
+            store.dispatch('addNodes', children);
+          })
+          .catch(e => console.error(e));}
+      }
+    });
+  }
+
+  graphChange() {
+    this.graph.getChildren(['hasContext']).then(children => {
+      for (let i = 0; i < children.length; i++) {
+        if (children[i].info.name.get() !== 'BIMObjectContext' && !this.nodes.includes(children[i])){
+          this.nodes.push(children[i]);
+          console.log(children[i].info.name.get(),children[i].info.name.get() === 'BIMObjectContext');
+        }
+      }
+
+      if (this.nodes.length > 0)
+      {this.store.dispatch("addContexts", this.nodes);}
+      this.store.dispatch("retrieveGlobalBar", this.graph);
+      this.store.dispatch("setGraph", this.graph);
+    });
+  }
+}
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import GraphManagerVue from "spinal-env-viewer-plugin-graph-manager-vue"
-import GraphManagerCo from "./GraphManager.js"
-import {SpinalForgeExtention} from "spinal-env-viewer-panel-manager-service_spinalforgeextention"
+import GraphManagerVue from "spinal-env-viewer-plugin-graph-manager-vue";
+import GraphManagerCo from "./GraphManager.js";
+import {SpinalForgeExtention} from "spinal-env-viewer-panel-manager-service_spinalforgeextention";
 
 
 const extentions = SpinalForgeExtention.createExtention({
-    name: "plugin-graph-manager",
-    vueMountComponent: GraphManagerVue.Component,
-    toolbar: {
-        //TODO find proper icon
-        icon: "view_compact",
-        label: "Graph Manager",
-        subToolbarName: "spinalcom"
-    },
-    panel: {
-        title: "Graph Manager",
-        className: "plugin-graph-viewer",
-        closeBehaviour: "hide"
-    },
-    style: {
-        height: '80vh'
-    },
-    onLoad: () => {
-        new GraphManagerCo(GraphManagerVue.Store);
-    }
+  name: "plugin-graph-manager",
+  vueMountComponent: GraphManagerVue.Component,
+  toolbar: {
+    //TODO find proper icon
+    icon: "view_compact",
+    label: "Graph Manager",
+    subToolbarName: "spinalcom"
+  },
+  panel: {
+    title: "Graph Manager",
+    className: "plugin-graph-viewer",
+    closeBehaviour: "hide"
+  },
+  style: {
+    height: '80vh'
+  },
+  onLoad: () => {
+    new GraphManagerCo(GraphManagerVue.Store);
+  }
 });
 SpinalForgeExtention.registerExtention("plugin-graph-manager", extentions);
